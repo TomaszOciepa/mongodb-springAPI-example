78806e1d3da5c52a9dfd64375dd2aa8eed45a033
tomszociepa <tom.ociepa@gmail.com>
2023-01-15 13:45:51 +0100

    create docker yaml

---
+++ src/main/java/com/tom/SpringBootRestApiMongodb/SpringBootRestApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.tom.SpringBootRestApiMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tom/SpringBootRestApiMongodb/SpringBootRestApiMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.SpringBootRestApiMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
