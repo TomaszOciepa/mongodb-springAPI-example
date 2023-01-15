3be5418ed7e55fb45076eddc3ca47979237b4233
tomszociepa <tom.ociepa@gmail.com>
2023-01-15 15:25:43 +0100

    student repository

--- src/main/java/com/tom/SpringBootRestApiMongodb/SpringBootRestApiMongodbApplication.java
+++ src/main/java/com/tom/SpringBootRestApiMongodb/SpringBootRestApiMongodbApplication.java
@@ -1,7 +1,14 @@
 package com.tom.SpringBootRestApiMongodb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootRestApiMongodbApplication {
@@ -9,5 +16,30 @@ public class SpringBootRestApiMongodbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiMongodbApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner(StudentRepository repository){
+		return args -> {
+			Address address = new Address(
+					"Poland",
+					"Gdańsk",
+					"80-283"
+			);
+
+			Student student = new Student(
+					"tomek",
+					"ociepa",
+					"tom@gmail.com",
+					Gender.MALE,
+					address,
+					List.of("Computer Science"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+
+			repository.insert(student);
+		};
+	}
+
 
 }
