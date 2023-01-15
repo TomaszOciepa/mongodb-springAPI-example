ca8deead44496106024b4b3213918c3f50c6f728
tomszociepa <tom.ociepa@gmail.com>
2023-01-15 14:53:25 +0100

    @Id for students model

--- src/main/java/com/tom/SpringBootRestApiMongodb/Student.java
+++ src/main/java/com/tom/SpringBootRestApiMongodb/Student.java
@@ -1,13 +1,18 @@
 package com.tom.SpringBootRestApiMongodb;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Data
+@Document
 public class Student {
+    @Id
+    private String id;
     private String name;
     private String lastname;
     private String email;
